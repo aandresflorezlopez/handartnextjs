23f20d67c6cf81f70cbae2e0e9a57aa56570c90b
Andres Florez Lopez <aandresflorezlopez@gmail.com>
2023-02-20 20:17:35 -0500

    pwa initial configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});

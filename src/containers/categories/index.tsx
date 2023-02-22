a099b72bf6fab1fe033d90fd025689358fd3e55f
Andres Florez Lopez <aandresflorezlopez@gmail.com>
2023-02-21 21:13:20 -0500

    add main components in home page

---
+++ src/containers/categories/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@mui/material";
+
+const Item = (props: {
+  item: {
+    name: string;
+    description: string;
+  };
+}) => {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.description}</p>
+
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
+  );
+};
+
+const Categories = (props: any) => {
+  const items = [
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!",
+    },
+    {
+      name: "Random Name #2",
+      description: "Hello World!",
+    },
+  ];
+
+  return (
+    <Carousel>
+      {items.map((item, i) => (
+        <Item key={i} item={item} />
+      ))}
+    </Carousel>
+  );
+};
+
+export default Categories;
